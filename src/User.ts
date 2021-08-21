8952bc36f5dfb34816896d88cfb9f32f596a975b
Leftyxiv <manny@aledoux.net>
2021-08-21 08:27:12 -0400

    extracted events logic into it's own class

--- src/User.ts
+++ src/User.ts
@@ -6,10 +6,8 @@ interface UserProps {
   age?: number;
 }
 
-type Callback = () => void;
-export class User {
-  events: {[key: string]: Callback[]} = {};
 
+export class User {
   constructor(private data: UserProps) {};
 
   get(propName: string): (number | string) {
@@ -18,19 +16,7 @@ export class User {
   set(update: UserProps): void {
     Object.assign(this.data, update);
   }
-  on(eventName: string, callback: Callback): void {
-    const handlers = this.events[eventName] || [];
-    handlers.push(callback)
-    this.events[eventName] = handlers;
-  }
-  trigger(eventName: string): void {
-    const handlers = this.events[eventName];
 
-    if (!handlers || handlers.length === 0) {
-      return;
-    }
-    handlers.forEach(handler => handler());
-  }
 
   fetch(): void {
     axios.get(`http://localhost:3000/users/${this.get('id')}`)
