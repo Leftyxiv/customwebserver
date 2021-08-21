eaa09ed0a0b4539e5f9da4e57a3b28d60e365405
Leftyxiv <manny@aledoux.net>
2021-08-21 14:02:25 -0400

    added attributes to the user class

--- src/User.ts
+++ src/User.ts
@@ -1,5 +1,6 @@
 import { Eventing } from './models/Eventing';
 import { Sync } from './models/Sync';
+import { Attributes } from './models/Attributes';
 
 export interface UserProps {
   id?: number;
@@ -11,4 +12,9 @@ const rootUrl = 'http://127.0.0.1:3000/users';
 export class User {
   events: Eventing = new Eventing();
   sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
+  public attributes: Attributes<UserProps>;
+
+  constructor(props?: UserProps) {
+    this.attributes = new Attributes<UserProps>(props);
+  }
 }
